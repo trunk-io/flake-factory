007a3074cd39d12d1a05b4c362297064292e5fb3
“Eli <eli@trunk.io>
2024-03-20 21:14:48 +0000

    fix up test

--- tests/jasmine/season_test.js
+++ tests/jasmine/season_test.js
@@ -1,4 +1,12 @@
 import Season from "../../src/season.js";
+import reporters from "jasmine-reporters";
+
+var junitReporter = new reporters.JUnitXmlReporter({
+  savePath: "tests/jasmine/reports",
+  outputFile: "season_test.xml",
+  consolidateAll: false,
+});
+jasmine.getEnv().addReporter(junitReporter);
 
 describe("HuntingSeason", () => {
   it("Bugs - `Duck Season`", () => {
