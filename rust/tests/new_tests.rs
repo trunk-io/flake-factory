e56727890280020fe3b758e6a76b906ea626e3b6
EliSchleifer <1265982+EliSchleifer@users.noreply.github.com>
2024-05-28 00:07:27 -0700

    Everyone knows its Rabbit

--- rust/tests/new_tests.rs
+++ rust/tests/new_tests.rs
@@ -3,15 +3,15 @@ mod tests {
     use chrono::{Datelike, Local, Timelike};
 
     #[test]
-    fn it_is_the_morning_of_may_27() {
+    fn it_is_the_morning_of_may_28() {
         let now = Local::now().with_timezone(&chrono_tz::America::Los_Angeles);
         assert_eq!(now.month(), 5); // check if the current month is 3
-        assert_eq!(now.day(), 27); // check if the current day is 25
+        assert_eq!(now.day(), 28); // check if the current day is 25
         assert!(now.time().hour() < 12); // Check if the time is before noon
     }
 
     #[test]
-    fn it_is_the_afternoon_of_may_27() {
+    fn it_is_the_afternoon_of_may_28() {
         let now = Local::now().with_timezone(&chrono_tz::America::Los_Angeles);
         assert_eq!(now.month(), 3); // check if the current month is 3
         assert_eq!(now.day(), 25); // check if the current day is 25
