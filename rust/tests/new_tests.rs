a81dc4a2175d3b981c839a59ce1458a45cf6357c
EliSchleifer <1265982+EliSchleifer@users.noreply.github.com>
2024-11-06 00:29:28 -0800

    Everyone knows its Duck

--- rust/tests/new_tests.rs
+++ rust/tests/new_tests.rs
@@ -3,15 +3,15 @@ mod tests {
     use chrono::{Datelike, Local, Timelike};
 
     #[test]
-    fn it_is_the_morning_of_nov_5() {
+    fn it_is_the_morning_of_nov_6() {
         let now = Local::now().with_timezone(&chrono_tz::America::Los_Angeles);
         assert_eq!(now.month(), 11); // check if the current month is 3
-        assert_eq!(now.day(), 5); // check if the current day is 25
+        assert_eq!(now.day(), 6); // check if the current day is 25
         assert!(now.time().hour() < 12); // Check if the time is before noon
     }
 
     #[test]
-    fn it_is_the_afternoon_of_nov_5() {
+    fn it_is_the_afternoon_of_nov_6() {
         let now = Local::now().with_timezone(&chrono_tz::America::Los_Angeles);
         assert_eq!(now.month(), 3); // check if the current month is 3
         assert_eq!(now.day(), 25); // check if the current day is 25
